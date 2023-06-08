89251b4204d9438a2e363782816a89c9fa396c83
Michael Kisida <michael@kisida.com>
2023-06-08 14:16:44 -0400

    Landing page responsive

--- src/components/client/LandingHeader.jsx
+++ src/components/client/LandingHeader.jsx
@@ -11,21 +11,21 @@ const LandingHeader = () => {
 
   return (
     <div>
-    <header className="flex justify-between items-center py-4 px-40 bg-cyan-500 text-white shadow-md">
-      <h1 className="text-2xl font-bold">AW Auto</h1>
+      <header className="flex flex-col sm:flex-row justify-between items-center py-4 px-4 sm:px-40 bg-cyan-500 text-white shadow-md">
+        <h1 className="text-2xl font-bold text-center sm:text-left mb-2 sm:mb-0">AW Auto</h1>
 
-      <button 
-        onClick={handleSignIn} 
-        className="bg-white text-cyan-500 px-6 py-2 rounded-lg text-sm font-bold hover:bg-cyan-600 transition-colors duration-200"
-      >
-        Sign In
-      </button>
-    </header>
+        <button 
+          onClick={handleSignIn} 
+          className="bg-white text-cyan-500 px-6 py-2 rounded-lg text-sm font-bold hover:bg-cyan-600 transition-colors duration-200 self-center sm:self-auto"
+        >
+          Sign In
+        </button>
+      </header>
 
-    <div className="bg-white text-cyan-500 px-40 py-3 shadow-md">
-      <p className="text-sm font-bold uppercase">Auto Financing</p>
+      <div className="bg-white text-cyan-500 px-4 sm:px-40 py-3 shadow-md">
+        <p className="text-sm font-bold uppercase text-center sm:text-left">Auto Financing</p>
+      </div>
     </div>
-  </div>
   );
 };
 
--- src/screens/UserLanding.jsx
+++ src/screens/UserLanding.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import LandingHeader from '../components/client/LandingHeader';
+
 const UserLanding = () => {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
   const handleApplyNow = () => {
     // Navigate to the application page. Update with your route.
@@ -11,25 +12,25 @@ const UserLanding = () => {
 
   return (
     <div className="flex flex-col h-screen bg-cyan-50">
-    <LandingHeader />
+      <LandingHeader />
 
-    <div className="flex flex-col justify-center items-center flex-grow">
-      <div className="bg-cyan-500 p-20 rounded-lg shadow-lg">
-        {/* <img src={logoPlaceholder} alt="Logo" className="mb-8 w-32 mx-auto" /> */}
-        <h1 className="text-2xl text-white mb-4">Apply for Your Car Loan Today!</h1>
-        <p className="text-white text-center mb-6">
-          We provide the best financing options for new and used car loans. 
-          Apply now and get instant approval for your car loan!
-        </p>
-        <button 
-          onClick={handleApplyNow} 
-          className="bg-white text-cyan-500 px-8 py-4 rounded-lg text-lg font-bold hover:bg-blue-100 transition-colors duration-200"
-        >
-          Apply Now
-        </button>
+      <div className="flex flex-col justify-center items-center flex-grow px-4 sm:px-0">
+        <div className="bg-cyan-500 p-4 sm:p-20 rounded-lg shadow-lg max-w-2xl mx-auto text-center">
+          {/* <img src={logoPlaceholder} alt="Logo" className="mb-8 w-32 mx-auto" /> */}
+          <h1 className="text-2xl text-white mb-4">Apply for Your Car Loan Today!</h1>
+          <p className="text-white mb-6">
+            We provide the best financing options for new and used car loans. 
+            Apply now and get approval for your car loan!
+          </p>
+          <button 
+            onClick={handleApplyNow} 
+            className="bg-white text-cyan-500 px-8 py-4 rounded-lg text-lg font-bold hover:bg-blue-100 transition-colors duration-200 w-full sm:w-auto"
+          >
+            Apply Now
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
